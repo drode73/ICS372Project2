c7561ac7462dc0d742284ccf54b58982ab66444c
Joshua Vang <vangjosh95@gmail.com>
2021-11-20 10:34:45 -0600

    Create the Interface for the GUIDisplay

--- Remote Control FX/edu.ics372.remotecontrolfx/edu/ics372/remotecontrolfx/display/GUIDisplay.java
+++ Remote Control FX/edu.ics372.remotecontrolfx/edu/ics372/remotecontrolfx/display/GUIDisplay.java
@@ -1,12 +1,25 @@
 package edu.ics372.remotecontrolfx.display;
 
+import edu.ics372.remotecontrolfx.buttons.FastForwardButton;
 import edu.ics372.remotecontrolfx.buttons.GUIButton;
+import edu.ics372.remotecontrolfx.buttons.OffButton;
+import edu.ics372.remotecontrolfx.buttons.OnButton;
+import edu.ics372.remotecontrolfx.buttons.PauseButton;
+import edu.ics372.remotecontrolfx.buttons.PlayButton;
+import edu.ics372.remotecontrolfx.buttons.RewindButton;
+import edu.ics372.remotecontrolfx.buttons.StopButton;
 import edu.ics372.remotecontrolfx.select.Show;
+import edu.ics372.remotecontrolfx.states.PlayerContext;
 import javafx.application.Application;
+import javafx.beans.value.ObservableValue;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.layout.GridPane;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 
 public class GUIDisplay extends Application implements VideoPlayerDisplay {
 
@@ -23,6 +36,66 @@ public class GUIDisplay extends Application implements VideoPlayerDisplay {
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
+		playerOn = new OnButton("On");
+		playerOff = new OffButton("Off");
+		playShow = new PlayButton("Play");
+		stopShow = new StopButton("Stop");
+		pauseShow = new PauseButton("Pause");
+		fastforwardShow = new FastForwardButton("FF");
+		rewindShow = new RewindButton("Rew");
+
+		// Current Status of the Player
+		statusText = new Label("Status: Player off");
+		statusText.setMinWidth(50);
+		statusText.setMinHeight(50);
+
+		// Create the ShowList
+		showList = new ListView<Show>();
+		GridPane mainPane = new GridPane();
+		mainPane.setHgap(10);
+		mainPane.setVgap(10);
+		GridPane statusPane = new GridPane();
+		GridPane buttonPane = new GridPane();
+
+		// Add buttons to the Left-side of the panel
+		buttonPane.add(playerOn, 0, 0);
+		buttonPane.add(playerOff, 0, 1);
+		buttonPane.add(playShow, 0, 2);
+		buttonPane.add(stopShow, 0, 3);
+		buttonPane.add(pauseShow, 0, 4);
+		buttonPane.add(fastforwardShow, 0, 5);
+		buttonPane.add(rewindShow, 0, 6);
+
+		// to add list view control to status pane
+		statusPane.add(statusText, 0, 0);
+		statusPane.add(showList, 0, 1);
+		showList.getItems().add(new Show("n1", 10));
+		showList.getItems().add(new Show("n2", 20));
+		showList.getItems().add(new Show("n3", 30));
+		showList.getItems().add(new Show("n4", 40));
+		showList.getItems().add(new Show("n5", 50));
+		showList.getSelectionModel().selectedItemProperty()
+				.addListener((ObservableValue<? extends Show> ov, Show old_val, Show new_val) -> {
+					Show selectedShow = showList.getSelectionModel().getSelectedItem();
+					PlayerContext.getInstance().onSelectShowRequest(selectedShow);
+				});
+		statusPane.setStyle("-fx-background-color: white;");
+		statusText.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
+		mainPane.add(buttonPane, 0, 0);
+		mainPane.add(statusPane, 1, 0, 5, 1);
+		statusPane.setVgap(10);
+		buttonPane.setVgap(10);
+		Scene scene = new Scene(mainPane);
+		primaryStage.setScene(scene);
+		primaryStage.setTitle("Remote Control and Player Interface");
+		PlayerContext.getInstance().setDisplay(this);
+		primaryStage.show();
+		primaryStage.addEventHandler(WindowEvent.WINDOW_CLOSE_REQUEST, new EventHandler<WindowEvent>() {
+			@Override
+			public void handle(WindowEvent window) {
+				System.exit(0);
+			}
+		});
 
 	}
 
