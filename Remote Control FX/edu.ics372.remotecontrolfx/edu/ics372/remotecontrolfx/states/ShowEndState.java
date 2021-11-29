f6f98bd6ef5fda8509c0555cf93bdb0ead23e4de
Joshua Vang <vangjosh95@gmail.com>
2021-11-29 16:35:41 -0600

    Writing more code to the ShowEndState

--- Remote Control FX/edu.ics372.remotecontrolfx/edu/ics372/remotecontrolfx/states/ShowEndState.java
+++ Remote Control FX/edu.ics372.remotecontrolfx/edu/ics372/remotecontrolfx/states/ShowEndState.java
@@ -1,5 +1,56 @@
 package edu.ics372.remotecontrolfx.states;
 
-public class ShowEndState {
+import edu.ics372.remotecontrolfx.timer.Notifiable;
+import edu.ics372.remotecontrolfx.timer.Timer;
+
+public class ShowEndState extends PlayerState implements Notifiable {
+
+	private static ShowEndState instance;
+	private Timer timer = null;
+
+	private ShowEndState() {
+
+	}
+
+	public static ShowEndState getInstance() {
+		if (instance == null) {
+			instance = new ShowEndState();
+		}
+		return instance;
+	}
+
+	public void playRequest() {
+		PlayerContext.getInstance().changeState(PlayingState.getInstance());
+	}
+
+	@Override
+	public void enter() {
+		timer = new Timer(this, 10);
+		PlayerContext.getInstance().showUnselected();
+
+	}
+
+	@Override
+	public void leave() {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void OnTimerTick(int timerValue) {
+		PlayerContext.getInstance().showTimeLeft(timerValue);
+
+	}
+
+	@Override
+	public void onTimerRunsOut() {
+		PlayerContext.getInstance().changeState(ScreenSaverState.getInstance());
+
+	}
+
+	@Override
+	public void onVideoRunsOut() {
+		// TODO Auto-generated method stub
+
+	}
 
 }
