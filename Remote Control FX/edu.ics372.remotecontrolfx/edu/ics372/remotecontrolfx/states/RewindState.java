4002cc95e69ad9990ae274083c2611054ed03b31
GameyMonster <44377094+GameyMonster@users.noreply.github.com>
2021-12-04 14:33:29 -0600

    Update RewindState.java

--- Remote Control FX/edu.ics372.remotecontrolfx/edu/ics372/remotecontrolfx/states/RewindState.java
+++ Remote Control FX/edu.ics372.remotecontrolfx/edu/ics372/remotecontrolfx/states/RewindState.java
@@ -1,55 +1,119 @@
 package edu.ics372.remotecontrolfx.states;
 
+/**
+ * @author Donald Roden
+ * @Copyright (c) 2021
+ */
+import edu.ics372.remotecontrolfx.collections.Show;
 import edu.ics372.remotecontrolfx.timer.Notifiable;
+import edu.ics372.remotecontrolfx.timer.Timer;
 
+/**
+ * Represents the Rewind button state
+ *
+ */
 public class RewindState extends PlayerState implements Notifiable {
-	
-	private Timer timer;
+	private static RewindState instance;
+	private Timer timer = null;
+	private Show show;
+	private int timeLeft;
 
-	private PlayerState instance;
-	private Timer timer;
-	
+	/**
+	 * Private constructor for the singleton pattern
+	 */
 	private RewindState() {
-		
+
 	}
 
-	private RewindState getInstance() {
+	/**
+	 * returns the instance
+	 * 
+	 * @return this object
+	 */
+	public static RewindState getInstance() {
 		if (instance == null) {
 			instance = new RewindState();
 		}
 		return instance;
 	}
-		
+
+	/**
+	 * Handle Off request
+	 * 
+	 */
+	@Override
+	public void offRequest() {
+		PlayerContext.getInstance().changeState(OffState.getInstance());
+	}
+
+	/**
+	 * Handle Play request
+	 * 
+	 */
+	@Override
+	public void playRequest() {
+		PlayerContext.getInstance().setShow(show);
+		PlayerContext.getInstance().changeState(PlayingState.getInstance());
+	}
+
+	/**
+	 * Handle Stop request
+	 * 
+	 */
+	@Override
+	public void stopRequest() {
+		PlayerContext.getInstance().changeState(ShowEndState.getInstance());
+	}
+
+	/**
+	 * Handle the Timer-tick request
+	 * 
+	 */
+
 	@Override
 	public void OnTimerTick(int timerValue) {
-		PlayerContext.getInstance().showTimeLeft(timerValue);
+		timeLeft += 2;
+		show.setElapsedTime(show.getElapsedTime() - 2);
+		if (timeLeft > show.getShowLength()) {
+			timeLeft = show.getShowLength();
+		}
+
+		PlayerContext.getInstance().showTimeLeft(timeLeft);
 	}
 
 	@Override
 	public void onTimerRunsOut() {
-		ScreenSaverState.getInstance().setPreviousState(UnselectState.getInstance());
-		PlayerContext.getInstance().changeState(ScreenSaverState.getInstance());
+		show.setElapsedTime(0);
+		PlayerContext.getInstance().changeState(SelectState.getInstance());
+	}
 
+	/**
+	 * Initialize the state
+	 */
+	@Override
+	public void enter() {
+		// timeLeft = show.getShowLength() - show.getElapsedTime();
+		show = PlayerContext.getInstance().getShow();
+		int time = (show.getElapsedTime() % 2 == 0) ? (show.getElapsedTime() / 2) : (show.getElapsedTime() / 2) + 1;
+		timer = new Timer(this, time);
+		timeLeft = show.getShowLength() - show.getElapsedTime();
+		PlayerContext.getInstance().showRewind();
 	}
 
 	@Override
 	public void leave() {
 		timer.stop();
-		PlayerContext.getInstance().
+		timer = null;
+		PlayerContext.getInstance().setShow(show);
 
 	}
 
-	@Override
-	public void enter() {
-
-		PlayerContext.getInstance().Rewind();
-		PlaverContext.getInstance().showRewind();
-
-
+	/**
+	 * Set the show to Rewind
+	 * 
+	 * @param show
+	 */
+	public void setShow(Show show) {
+		this.show = show;
 	}
-	
-	public void offRequest() {
-		PlayerContext.getInstance().changeState(OffState.getInstance());
-	}
-
 }
