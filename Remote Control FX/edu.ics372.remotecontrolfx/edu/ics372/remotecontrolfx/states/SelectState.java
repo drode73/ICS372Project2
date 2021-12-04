98dbf339a72ff383eb9ede5b3f0e53996945395b
GameyMonster <44377094+GameyMonster@users.noreply.github.com>
2021-12-04 14:33:58 -0600

    Update SelectState.java

--- Remote Control FX/edu.ics372.remotecontrolfx/edu/ics372/remotecontrolfx/states/SelectState.java
+++ Remote Control FX/edu.ics372.remotecontrolfx/edu/ics372/remotecontrolfx/states/SelectState.java
@@ -1,16 +1,34 @@
 package edu.ics372.remotecontrolfx.states;
 
+/**
+ * @author Kyle McClernon
+ * @Copyright (c) 2021
+ */
+import edu.ics372.remotecontrolfx.collections.Show;
 import edu.ics372.remotecontrolfx.timer.Notifiable;
 import edu.ics372.remotecontrolfx.timer.Timer;
 
+/**
+ * Represents the Select state
+ *
+ */
 public class SelectState extends PlayerState implements Notifiable {
 	private static SelectState instance;
+	private Show show;
 	private Timer timer;
 
+	/**
+	 * Private constructor for singleton pattern.
+	 */
 	private SelectState() {
 
 	}
 
+	/**
+	 * returns the instance
+	 * 
+	 * @return this object
+	 */
 	public static SelectState getInstance() {
 		if (instance == null) {
 			instance = new SelectState();
@@ -19,60 +37,108 @@ public class SelectState extends PlayerState implements Notifiable {
 		return instance;
 	}
 
+	/**
+	 * Handle On request
+	 * 
+	 */
 	@Override
-	public void OnTimerTick(int timerValue) {
-		// TODO Auto-generated method stub
+	public void onRequest() {
+		timer.setTimeValue(10);
+	}
 
+	/**
+	 * Handle Off request
+	 * 
+	 */
+	@Override
+	public void offRequest() {
+		PlayerContext.getInstance().changeState(OffState.getInstance());
+	}
+
+	/**
+	 * Handle Select request
+	 * 
+	 */
+	@Override
+	public void selectRequest(Show show) {
+		PlayerContext.getInstance().setShow(show);
+		timer.setTimeValue(10);
+		PlayerContext.getInstance().showSelected();
+	}
+
+	/**
+	 * Handle Play request
+	 * 
+	 */
+	@Override
+	public void playRequest() {
+		PlayerContext.getInstance().changeState(PlayingState.getInstance());
+	}
+
+	/**
+	 * Handle Pause request
+	 * 
+	 */
+	@Override
+	public void pauseRequest() {
+		timer.setTimeValue(10);
+	}
+
+	/**
+	 * Handle Stop request
+	 * 
+	 */
+	@Override
+	public void stopRequest() {
+		timer.setTimeValue(10);
+	}
+
+	/**
+	 * Handle Rewind request
+	 * 
+	 */
+	@Override
+	public void rewindRequest() {
+		timer.setTimeValue(10);
+	}
+
+	/**
+	 * Handle Fast-forward request
+	 * 
+	 */
+	@Override
+	public void fastFowardRequest() {
+		timer.setTimeValue(10);
+	}
+
+	/**
+	 * Handle the Timer-tick request
+	 * 
+	 */
+	@Override
+	public void onTimerRunsOut() {
+		ScreenSaverState.getInstance().setPreviousState(this);
+		PlayerContext.getInstance().changeState(ScreenSaverState.getInstance());
 	}
 
 	@Override
-	public void onTimerRunsOut() {
+	public void OnTimerTick(int timerValue) {
 		// TODO Auto-generated method stub
+	}
 
+	/**
+	 * Initialize the state
+	 */
+	@Override
+	public void enter() {
+		timer = new Timer(this, 10);
+		PlayerContext.getInstance().showSelected();
 	}
 
 	@Override
 	public void leave() {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void enter() {
-		// TODO Auto-generated method stub
-
-	}
-	
-	public void onRequest() {
-		PlayerContext.getInstance().changeState(SelectState.getInstance());
-	}
-	
-	public void offRequest() {
-		PlayerContext.getInstance().changeState(OffState.getInstance());
-	}
-	
-	public void selectRequest() {
-		PlayerContext.getInstance().changeState(SelectState.getInstance());
-	}
-	
-	public void playRequest() {
-		PlayerContext.getInstance().changeState(PlayingState.getInstance());
-	}
-	
-	public void pauseRequest() {
-		PlayerContext.getInstance().changeState(PauseState.getInstance());
-	}
-	
-	public void stopRequest() {
-		PlayerContext.getInstance().changeState(ShowEndState.getInstance());
-	}
-	
-	public void rewindRequest() {
-		PlayerContext.getInstance().changeState(RewindState.getInstance());
-	}
-	
-	public void fastForwardRequest() {
-		PlayerContext.getInstance().changeState(FastForwardState.getInstance());
+		timer.stop();
+		timer = null;
 	}
 
 }
