354f1c322ff0e91d730ca90b4e240bd111710209
GameyMonster <44377094+GameyMonster@users.noreply.github.com>
2021-12-04 14:31:04 -0600

    Update FastForwardState.java

--- Remote Control FX/edu.ics372.remotecontrolfx/edu/ics372/remotecontrolfx/states/FastForwardState.java
+++ Remote Control FX/edu.ics372.remotecontrolfx/edu/ics372/remotecontrolfx/states/FastForwardState.java
@@ -1,51 +1,126 @@
 package edu.ics372.remotecontrolfx.states;
 
-
-// Test commit
-
+/**
+ * @author Donald Roden
+ * @Copyright (c) 2021
+ */
+import edu.ics372.remotecontrolfx.collections.Show;
 import edu.ics372.remotecontrolfx.timer.Notifiable;
+import edu.ics372.remotecontrolfx.timer.Timer;
 
+/**
+ * Represents the Fast-Forward state
+ *
+ */
 public class FastForwardState extends PlayerState implements Notifiable {
-	
-	private PlayerState instance;
-	private Timer timer;
-	
+
+	private static FastForwardState instance;
+	private Timer timer = null;
+	private Show show;
+	private int timeLeft;
+
+	/**
+	 * Private constructor for the singleton pattern
+	 */
 	private FastForwardState() {
-		
+
 	}
 
-	private FastForwardState getInstance() {
+	/**
+	 * returns the instance
+	 * 
+	 * @return this object
+	 */
+	public static FastForwardState getInstance() {
 		if (instance == null) {
 			instance = new FastForwardState();
 		}
 		return instance;
-
-	@Override
-	public void OnTimerTick(int timerValue) {
-			PlayerContext.getInstance().showTimeLeft(timerValue);
-
 	}
 
+	/**
+	 * Handle Off request
+	 * 
+	 */
+
 	@Override
-	public void onTimerRunsOut() {
-		ScreenSaverState.getInstance().setPreviousState(UnselectState.getInstance());
-		PlayerContext.getInstance().changeState(ScreenSaverState.getInstance());
+	public void offRequest() {
+		PlayerContext.getInstance().changeState(OffState.getInstance());
+	}
+
+	/**
+	 * Handle Play request
+	 * 
+	 */
+
+	@Override
+	public void playRequest() {
+		PlayerContext.getInstance().changeState(PlayingState.getInstance());
+	}
+
+	/**
+	 * Handle Stop request
+	 * 
+	 */
+
+	@Override
+	public void stopRequest() {
+		PlayerContext.getInstance().changeState(ShowEndState.getInstance());
+	}
+
+	/**
+	 * Initialize the state
+	 */
+	@Override
+	public void enter() {
+		show = PlayerContext.getInstance().getShow();
+		timeLeft = show.getShowLength() - show.getElapsedTime();
+		int time = (timeLeft % 2 == 0) ? (timeLeft / 2) : (timeLeft / 2) + 1;
+		timer = new Timer(this, time);
+		PlayerContext.getInstance().showFastForward();
 	}
 
 	@Override
 	public void leave() {
 		timer.stop();
-		PlayerContext.getInstance().
+		timer = null;
+		PlayerContext.getInstance().setShow(show);
+
+	}
+
+	/**
+	 * Get the show names
+	 * 
+	 * @return
+	 */
+	public Show getShow() {
+		return show;
+	}
+
+	public void setShow(Show show) {
+		this.show = show;
+	}
+
+	/**
+	 * Handle the Timer-tick request
+	 * 
+	 */
+
+	@Override
+	public void OnTimerTick(int timerValue) {
+		timeLeft -= 2;
+		show.setElapsedTime(show.getElapsedTime() + 2);
+		if (timeLeft < 0) {
+			timeLeft = 0;
+		}
+		PlayerContext.getInstance().showTimeLeft(timeLeft);
 
 	}
 
 	@Override
-	public void enter() {
-		PlaverContext.getInstance().showFastForward();
-	}
-	
-	public void offRequest() {
-		PlayerContext.getInstance().changeState(OffState.getInstance());
-	}
+	public void onTimerRunsOut() {
+		PlayerContext.getInstance().showTimeLeft(0);
+		PlayerContext.getInstance().changeState(ShowEndState.getInstance());
 
+	}
 }
