8c27fd4676f3b003910d2729bf15af86e561ef22
drode73 <80714092+drode73@users.noreply.github.com>
2021-12-04 14:02:28 -0600

    Kyles state

--- Remote Control FX/edu.ics372.remotecontrolfx/edu/ics372/remotecontrolfx/states/SelectState.java
+++ Remote Control FX/edu.ics372.remotecontrolfx/edu/ics372/remotecontrolfx/states/SelectState.java
@@ -42,5 +42,37 @@ public class SelectState extends PlayerState implements Notifiable {
 		// TODO Auto-generated method stub
 
 	}
+	
+	public void onRequest() {
+		PlayerContext.getInstance().changeState(SelectState.getInstance());
+	}
+	
+	public void offRequest() {
+		PlayerContext.getInstance().changeState(OffState.getInstance());
+	}
+	
+	public void selectRequest() {
+		PlayerContext.getInstance().changeState(SelectState.getInstance());
+	}
+	
+	public void playRequest() {
+		PlayerContext.getInstance().changeState(PlayingState.getInstance());
+	}
+	
+	public void pauseRequest() {
+		PlayerContext.getInstance().changeState(PauseState.getInstance());
+	}
+	
+	public void stopRequest() {
+		PlayerContext.getInstance().changeState(ShowEndState.getInstance());
+	}
+	
+	public void rewindRequest() {
+		PlayerContext.getInstance().changeState(RewindState.getInstance());
+	}
+	
+	public void fastForwardRequest() {
+		PlayerContext.getInstance().changeState(FastForwardState.getInstance());
+	}
 
 }
