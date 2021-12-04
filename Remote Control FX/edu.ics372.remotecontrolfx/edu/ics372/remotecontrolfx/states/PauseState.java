e860eda2714a117a5821ad7169d7c007fddc5636
GameyMonster <44377094+GameyMonster@users.noreply.github.com>
2021-12-04 14:31:51 -0600

    Update PauseState.java

--- Remote Control FX/edu.ics372.remotecontrolfx/edu/ics372/remotecontrolfx/states/PauseState.java
+++ Remote Control FX/edu.ics372.remotecontrolfx/edu/ics372/remotecontrolfx/states/PauseState.java
@@ -1,42 +1,92 @@
 package edu.ics372.remotecontrolfx.states;
 
+/**
+* @author Kyle McClernon
+* @Copyright (c) 2021
+*/
+import edu.ics372.remotecontrolfx.collections.Show;
+
+/**
+ * Represents the Pause Button state
+ *
+ */
 public class PauseState extends PlayerState {
 	private static PauseState instance;
-	private PlayerState previousState;
+	private Show show;
 
+	/**
+	 * Private constructor for the singleton pattern
+	 */
 	private PauseState() {
 
 	}
 
-	private PauseState getInstance() {
+	/**
+	 * Returns the instance
+	 * 
+	 * @return this object
+	 */
+	public static PauseState getInstance() {
 		if (instance == null) {
 			instance = new PauseState();
 		}
 		return instance;
 	}
 
+	/**
+	 * Handle Off request
+	 * 
+	 */
 	@Override
-	public void leave() {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void enter() {
-		PlayerContext.getInstance().showPaused();
-
-	}
-
 	public void offRequest() {
 		PlayerContext.getInstance().changeState(OffState.getInstance());
 	}
 
+	/**
+	 * Handle Play request
+	 * 
+	 */
+	@Override
 	public void playRequest() {
 		PlayerContext.getInstance().changeState(PlayingState.getInstance());
 	}
 
+	/**
+	 * Handle Stop request
+	 * 
+	 */
+	@Override
 	public void stopRequest() {
 		PlayerContext.getInstance().changeState(ShowEndState.getInstance());
+	}
+
+	/**
+	 * Initialize the state
+	 * 
+	 */
+	@Override
+	public void enter() {
+		this.show = PlayerContext.getInstance().getShow();
+		PlayerContext.getInstance().showPaused();
+
+	}
+
+	@Override
+	public void leave() {
+		PlayerContext.getInstance().setShow(show);
+	}
+
+	/**
+	 * Get the show names
+	 * 
+	 * @return
+	 */
+	public Show getShow() {
+		return show;
+	}
+
+	public void setShow(Show show) {
+		this.show = show;
 	}
 
 }
