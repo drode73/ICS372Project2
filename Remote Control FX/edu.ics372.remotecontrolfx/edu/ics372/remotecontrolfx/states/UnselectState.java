f716acedb590d706fa8fa4ffd235976bfc2e27fe
GameyMonster <44377094+GameyMonster@users.noreply.github.com>
2021-12-04 14:34:33 -0600

    Update UnselectState.java

--- Remote Control FX/edu.ics372.remotecontrolfx/edu/ics372/remotecontrolfx/states/UnselectState.java
+++ Remote Control FX/edu.ics372.remotecontrolfx/edu/ics372/remotecontrolfx/states/UnselectState.java
@@ -1,16 +1,33 @@
 package edu.ics372.remotecontrolfx.states;
 
+/**
+ * @author Kyle McClernon
+ * @Copyright (c) 2021
+ */
+import edu.ics372.remotecontrolfx.collections.Show;
 import edu.ics372.remotecontrolfx.timer.Notifiable;
 import edu.ics372.remotecontrolfx.timer.Timer;
 
+/**
+ * Represents the Unselect state
+ *
+ */
 public class UnselectState extends PlayerState implements Notifiable {
 	private static UnselectState instance;
 	private Timer timer;
 
+	/**
+	 * Private constructor for the singleton pattern
+	 */
 	private UnselectState() {
 
 	}
 
+	/**
+	 * returns the instance
+	 * 
+	 * @return this object
+	 */
 	public static UnselectState getInstance() {
 		if (instance == null) {
 			instance = new UnselectState();
@@ -18,39 +35,67 @@ public class UnselectState extends PlayerState implements Notifiable {
 		return instance;
 	}
 
+	/**
+	 * Handle On request
+	 * 
+	 */
 	public void onRequest() {
 		timer.setTimeValue(10);
 	}
 
+	/**
+	 * Handle Off request
+	 * 
+	 */
 	public void offRequest() {
 		PlayerContext.getInstance().changeState(OffState.getInstance());
 	}
 
+	/**
+	 * Handle Pause request
+	 * 
+	 */
 	public void pauseRequest() {
 		timer.setTimeValue(10);
 	}
 
-	public void selectRequest() {
+	/**
+	 * Handle Select request
+	 * 
+	 */
+	public void selectRequest(Show show) {
+		PlayerContext.getInstance().setShow(show);
 		PlayerContext.getInstance().changeState(SelectState.getInstance());
 	}
 
+	/**
+	 * Handle Play request
+	 * 
+	 */
 	public void playRequest() {
 		timer.setTimeValue(10);
 	}
 
+	/**
+	 * Handle Rewind request
+	 * 
+	 */
 	public void rewindRequest() {
 		timer.setTimeValue(10);
 	}
 
+	/**
+	 * Handle Fast-forward request
+	 * 
+	 */
 	public void fastFowardRequest() {
 		timer.setTimeValue(10);
 	}
 
-	@Override
-	public void OnTimerTick(int timerValue) {
-		// TODO Auto-generated method stub
-	}
-
+	/**
+	 * Handle the Timer-tick request
+	 * 
+	 */
 	@Override
 	public void onTimerRunsOut() {
 		// Screen saver State
@@ -59,12 +104,14 @@ public class UnselectState extends PlayerState implements Notifiable {
 		PlayerContext.getInstance().changeState(ScreenSaverState.getInstance());
 	}
 
-	public void leave() {
-		timer.stop();
-		timer = null;
-
+	@Override
+	public void OnTimerTick(int timerValue) {
+		// TODO Auto-generated method stub
 	}
 
+	/**
+	 * Initialize the state
+	 */
 	@Override
 	public void enter() {
 		timer = new Timer(this, 10);
@@ -72,4 +119,9 @@ public class UnselectState extends PlayerState implements Notifiable {
 
 	}
 
+	public void leave() {
+		timer.stop();
+		timer = null;
+
+	}
 }
